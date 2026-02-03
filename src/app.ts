439746571337c6891ba6f590964a3f16bc565a4a
CraneGuyLabs <rigging.integrated.group@gmail.com>
2026-02-03 16:04:06 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
+import express from "express";
+import v1Router from "./api/v1";
 
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// mount API
+app.use("/api/v1", v1Router);
+
+export default app;
