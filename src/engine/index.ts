380d05a501db9281673f995d0542ece468b48e46
CraneGuyLabs <rigging.integrated.group@gmail.com>
2026-02-09 11:50:29 -0600

    Update index.ts

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,1 +1,8 @@
-export { evaluateLift } from "./evaluateLift";
+/**
+ * ENGINE BARREL EXPORT
+ * --------------------
+ * This file exposes the authoritative Sling App engine entry point.
+ * No logic belongs here.
+ */
+
+export { runRiggingEngine } from "./engineEntry";
