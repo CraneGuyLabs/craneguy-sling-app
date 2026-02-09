9ac71235fc7e26f9fe59f02e1b0494d10e815857
CraneGuyLabs <rigging.integrated.group@gmail.com>
2026-02-09 12:48:41 -0600

    Update EngineOutput.ts

--- src/core/types/EngineOutput.ts
+++ src/core/types/EngineOutput.ts
@@ -1,1 +1,19 @@
+import { BottomRiggingResult } from "./BottomRiggingResult";
+import { TopRiggingResult } from "./TopRiggingResult";
+import { LateralPressureResult } from "./LateralPressureResult";
+import { BeamEvaluationResult } from "./BeamEvaluationResult";
+import { HookHeightInfo } from "./HookHeightInfo";
+import { GoverningSummary } from "./GoverningSummary";
+import { EngineInput } from "./EngineInput";
 
+export interface EngineOutput {
+  input: EngineInput;
+  bottomRigging: BottomRiggingResult;
+  lateralPressure: LateralPressureResult;
+  longerSlingMitigation: any | null;
+  beamEvaluation: BeamEvaluationResult;
+  topRigging: TopRiggingResult | null;
+  hookHeightInfo: HookHeightInfo;
+  governingSummary: GoverningSummary;
+  disclaimer: string;
+}
