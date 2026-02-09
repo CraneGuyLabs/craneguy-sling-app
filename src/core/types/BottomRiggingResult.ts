127de1a271fb1bf8ed60ea641ae553c1fc2388bc
CraneGuyLabs <rigging.integrated.group@gmail.com>
2026-02-09 12:42:10 -0600

    Update BottomRiggingResult.ts

--- src/core/types/BottomRiggingResult.ts
+++ src/core/types/BottomRiggingResult.ts
@@ -1,1 +1,31 @@
+export interface BottomRiggingResult {
+  legs: Array<{
+    legId: string;
+    angleFromHorizontal: number;
+    tension: number;
+    sling: {
+      type: string;
+      minimumWLL: number;
+      recommendedWLL: number;
+      selectedSize: string;
+      recommendedSize: string;
+      verticalRise?: number;
+    };
+    shackle: {
+      nominalSize: string;
+      wll: number;
+      weight: number;
+    };
+  }>;
 
+  governingLegId: string;
+  governingTension: number;
+  governingReason: string;
+
+  beam?: {
+    type: string;
+    wll: number;
+    weight: number;
+    height: number;
+  };
+}
