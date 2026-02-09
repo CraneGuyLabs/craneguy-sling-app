b53cc1e9187a9b0845890ab7f9c88dd11caad9c8
CraneGuyLabs <rigging.integrated.group@gmail.com>
2026-02-09 12:45:26 -0600

    Update BeamEvaluationResult.ts

--- src/core/types/BeamEvaluationResult.ts
+++ src/core/types/BeamEvaluationResult.ts
@@ -1,1 +1,8 @@
-
+export interface BeamEvaluationResult {
+  beamRequired: boolean;
+  beamType?: string;
+  beamWLL?: number;
+  beamWeight: number;
+  addedToRiggingWeight: boolean;
+  governingReason?: string;
+}
