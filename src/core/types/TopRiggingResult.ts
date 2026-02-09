c9c0ea68ed2b5a021a14eba55ff16c3a011d3d6b
CraneGuyLabs <rigging.integrated.group@gmail.com>
2026-02-09 12:43:21 -0600

    Update TopRiggingResult.ts

--- src/core/types/TopRiggingResult.ts
+++ src/core/types/TopRiggingResult.ts
@@ -1,1 +1,24 @@
+export interface TopRiggingResult {
+  legs: Array<{
+    legId: string;
+    angleFromHorizontal: number;
+    tension: number;
+    sling: {
+      type: string;
+      minimumWLL: number;
+      recommendedWLL: number;
+      selectedSize: string;
+      recommendedSize: string;
+      verticalRise?: number;
+    };
+    shackle: {
+      nominalSize: string;
+      wll: number;
+      weight: number;
+    };
+  }>;
 
+  governingLegId: string;
+  governingTension: number;
+  governingReason: string;
+}
